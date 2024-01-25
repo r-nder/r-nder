efc484f92bcbcbd61f461b3f0a09eeb914a883e9
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-01-25 20:04:32 +0100

    add basic window builder

--- crates/window/src/window.rs
+++ crates/window/src/window.rs
@@ -14,11 +14,76 @@ impl Default for Window {
     fn default() -> Self {
         Self {
             title: "App".to_owned(),
-            cursor: Default::default(),
+            cursor: Cursor::default(),
             visible: true,
             transparent: false,
             focused: true,
             resizeable: true,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct WindowBuilder {
+    title: String,
+    cursor: Cursor,
+    visible: bool,
+    transparent: bool,
+    focused: bool,
+    resizeable: bool,
+}
+
+impl WindowBuilder {
+    pub fn with_title(self, title: String) -> Self {
+        Self {
+            title,
+            ..self
+        }
+    }
+
+    pub fn with_cursor(self, cursor: Cursor) -> Self {
+        Self {
+            cursor,
+            ..self
+        }
+    }
+
+    pub fn set_visible(self, visible: bool) -> Self {
+        Self {
+            visible,
+            ..self
+        }
+    }
+
+    pub fn set_transparent(self, transparent: bool) -> Self {
+        Self {
+            transparent,
+            ..self
+        }
+    }
+
+    pub fn set_focused(self, focused: bool) -> Self {
+        Self {
+            focused,
+            ..self
+        }
+    }
+
+    pub fn set_resizeable(self, resizeable: bool) -> Self {
+        Self {
+            resizeable,
+            ..self
+        }
+    }
+
+    pub fn build(self) -> Window {
+        Window {
+            title: self.title,
+            cursor: self.cursor,
+            visible: self.visible,
+            transparent: self.transparent,
+            focused: self.focused,
+            resizeable: self.resizeable,
         }
     }
 }
